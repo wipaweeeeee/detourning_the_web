b54afaac5a73b7919bcfa13bb21bc94f636d3e6a
Howard Tam <howardhwtam@gmail.com>
2017-03-10 15:37:24 +0800

    clean up

--- showerThoughts.py
+++ showerThoughts.py
@@ -5,19 +5,16 @@ import os
 driver = webdriver.Firefox()
 
 url = 'https://www.reddit.com/r/Showerthoughts/'
-thoughts = []
+thoughts_arr = []
 
 def get_page():
 	time.sleep(2)
-
 	titles = driver.find_elements_by_css_selector('a.title')
 
 	for title in titles:
-		# print title.text
-		thoughts.append(title.text.encode('utf-8'))
+		thoughts_arr.append(title.text.encode('utf-8'))
 
 	driver.find_element_by_css_selector('span.next-button').click()
-
 
 driver.get(url)
 for i in range(10):
@@ -25,8 +22,9 @@ for i in range(10):
 driver.quit()
 
 
-# Please check if there's a 3-second pause between thoughts
-thoughts_str = " [[slnc 3000]] ".join(thoughts)
+# Please check if there's a 3-second pause between thoughts.
+# If script doesn't run, delete line 27 and uncomment 28.
+thoughts_str = " [[slnc 3000]] ".join(thoughts_arr)
 # thoughts_str = " ".join(thoughts)
 command = "say -v Alex \"%s\" -o shower.aiff" % 
 os.system(command)
