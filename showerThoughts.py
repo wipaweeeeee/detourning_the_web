a0a48976198e41e0a193c3a480bb91fe574100b6
Howard Tam <howardhwtam@gmail.com>
2017-03-11 21:04:17 +0800

    clean up

--- showerThoughts.py
+++ showerThoughts.py
@@ -10,7 +10,6 @@ thoughts_arr = []
 def get_page():
         time.sleep(2)
 	titles = driver.find_elements_by_css_selector('a.title')
-
 	for title in titles:
 		thoughts_arr.append(title.text.encode('utf-8'))
 
@@ -22,7 +21,6 @@ for i in range(10):
 	get_page()
         
 driver.quit()
-
 
 thoughts_str = " [[slnc 3000]] ".join(thoughts_arr)
 thoughts_str_clean = thoughts_str.replace("'", " ") # Look, I can explain...
